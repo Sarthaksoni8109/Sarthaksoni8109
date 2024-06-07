76d749cffc47cbb49e2a9b94d42e64d3518361f3
Sarthaksoni8109 <149762866+Sarthaksoni8109@users.noreply.github.com>
2024-06-08 02:12:45 +0530

    Java files

---
+++ Method_Parameters.java
@@ -0,0 +1,14 @@
+package Methods;
+
+public class Method_Parameters{
+    static void myMethod(String fname){
+        System.out.println(fname  + "->" + " next ");
+    }
+
+    public static void main(String[] args) {
+        myMethod("Sarthak");
+        myMethod("Amit");
+        myMethod("Mohan");
+        myMethod("Chandrashekhar");
+    }
+}
