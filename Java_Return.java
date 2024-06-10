3bd4709c2d8484f21d8f54126e543bc0fed8dd63
Sarthaksoni8109 <149762866+Sarthaksoni8109@users.noreply.github.com>
2024-06-10 16:08:12 +0530

    Java Codes

---
+++ Java_Return.java
@@ -0,0 +1,23 @@
+package Methods;
+
+public class Java_Return {
+    static int myMethod(int x) {
+        return 267 + x;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(myMethod(67));
+
+        //two parameters;
+        System.out.println(myMethod(5, 3));
+    }
+
+    static int myMethod(int v, int w) {
+        return v + w;
+
+    }
+}
+
+
+
+
